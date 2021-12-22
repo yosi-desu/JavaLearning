09f2f4a3ffd070f58e790d3c8d35d9dc8970bd62
yoshi_desu <xingdiansongtian@gmail.com>
2021-12-22 22:22:23 +0900

    補正

--- src/PictureBookOfJava/ControlStatement/FindPrimeNumber.java
+++ src/PictureBookOfJava/ControlStatement/FindPrimeNumber.java
@@ -2,21 +2,23 @@ package PictureBookOfJava.ControlStatement;
 
 public class FindPrimeNumber {
   public static void main(String[] args) {
-    int max =100;
+    int max =1000;
     boolean[] a = new boolean[max];
-    for(int i=0;i<max;i++)
+    for(int i=0;i<max;i++){
       a[i]=true;
+    }
      for(int i=2;i<max;i++){
        if (a[i-1]){
-         for (int j=2;i*j<=max;j++)
+         for (int j=2;i*j<=max;j++){
            a[i*j-1]=false;
          }
        }
-
-       for (int i =1;i<max;i++){
-         if (a[i])
-           System.out.println((i+1)+" ");
-       }
-       System.out.println();
      }
-    }
+     for (int i =1;i<max;i++){
+       if (a[i]){
+         System.out.println((i+1)+" ");
+       }
+     }
+     System.out.println();
+  }
+}
