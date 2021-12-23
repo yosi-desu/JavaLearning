402927ed07ed48ade20e06c8b3e2b870290ec97a
yoshi_desu <xingdiansongtian@gmail.com>
2021-12-23 20:48:24 +0900

    SortAlgorithm

--- src/PictureBookOfJava/ControlStatement/FindPrimeNumber.java
+++ src/PictureBookOfJava/ControlStatement/FindPrimeNumber.java
@@ -2,23 +2,24 @@ package PictureBookOfJava.ControlStatement;
 
 public class FindPrimeNumber {
   public static void main(String[] args) {
+    //素数を見つける範囲
     int max =1000;
     boolean[] a = new boolean[max];
     for(int i=0;i<max;i++){
       a[i]=true;
     }
-     for(int i=2;i<max;i++){
+    //素数を判断(倍数を見つける)
+    for(int i=2;i<max;i++){
        if (a[i-1]){
          for (int j=2;i*j<=max;j++){
            a[i*j-1]=false;
          }
        }
-     }
+    }
      for (int i =1;i<max;i++){
        if (a[i]){
-         System.out.println((i+1)+" ");
+         System.out.print((i+1)+" ");
        }
      }
-     System.out.println();
   }
 }
