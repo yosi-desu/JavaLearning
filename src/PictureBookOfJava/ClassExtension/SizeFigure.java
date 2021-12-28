72573477900649e87b8ec0cc4bc462126d4f037a
yoshi_desu <xingdiansongtian@gmail.com>
2021-12-28 20:22:30 +0900

    コードフォーマットの整形

--- src/PictureBookOfJava/ClassExtension/SizeFigure.java
+++ src/PictureBookOfJava/ClassExtension/SizeFigure.java
@@ -2,9 +2,9 @@ package PictureBookOfJava.ClassExtension;
 
 public class SizeFigure {
   public static void main(String[] args) {
-    Figure fig1= new Square(2.5);
-    Figure fig2= new Square(2.3,3.7);
-    Figure fig3=new Circle(3.6);
+    Figure fig1 = new Square(2.5);
+    Figure fig2 = new Square(2.3, 3.7);
+    Figure fig3 = new Circle(3.6);
     fig1.measure();
     fig2.measure();
     fig3.measure();
@@ -38,31 +38,31 @@ class Square extends Figure {
 
   @Override
   void area() {
-    System.out.println("四角形の面積:"+(side*height));
+    System.out.println("四角形の面積:" + (side * height));
   }
 
   @Override
   void around() {
-    System.out.println("四角形の外周:"+(2*(side+height)));
+    System.out.println("四角形の外周:" + (2 * (side + height)));
   }
 }
 
-class Circle extends Figure{
+class Circle extends Figure {
   final double pi = 3.14;
   double radius;
 
-  Circle(double r){
-    radius=r;
+  Circle(double r) {
+    radius = r;
   }
 
   @Override
   void area() {
-    System.out.println("円の面積:"+(radius*radius*pi));
+    System.out.println("円の面積:" + (radius * radius * pi));
   }
 
   @Override
   void around() {
-    System.out.println("円の外周:"+2*radius*pi);
+    System.out.println("円の外周:" + 2 * radius * pi);
   }
 }
 
